4e76e35293011f3cfd5ef88c0ff54aa1a47dfa82
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-15 11:51:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,9 +70,6 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
       image: image,
     })
     .then(function () {
-     console.log("1");
-      
-      console.log("2");
       return firebase.database().ref("subscriptions").once("value");
     })
     .then(function (subscriptions) {
@@ -88,7 +85,7 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
         const privkey = "AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s";
         const options = {
           vapidDetails: {
-            subject: 'abhishekgoyal274@gmail.com',
+            subject: 'https://pwa-course-79727.web.app',
             publicKey: pubkey,
             privateKey: privkey
           },
@@ -104,10 +101,10 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
             options
           )
           .then((res) => {
-            console.log("[Success]");
+            console.log("[Success]", res);
           })
           .catch(function (err) {
-            console.log("[Error in Web Push]");
+            console.log("[Error in Web Push]",err);
           });
       });
       response.status(201).json({
@@ -120,140 +117,6 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
       response.status(500).json({ error: err });
     });
 });
-
-// My Code
-// try {
-//   console.log(req.body);
-//   const post = {
-//     id: req.body.id,
-//     image: req.body.image,
-//     title: req.body.title,
-//     location: req.body.location,
-//   };
-//   const random =
-//     "posts/" + req.body.id + Math.random().toString(36).substring(2, 13);
-//   const response = await firebase.database().ref(random).set(post);
-//   res.send(response);
-// } catch (error) {
-//   res.send(error);
-// }
-// });
-  // var uuid = UUID();
-  // console.log(request.file);
-  // console.log(request.body);
-  // const busboy = Busboy({ headers: request.headers });
-  // // These objects will store the values (file + fields) extracted from busboy
-  // let upload;
-  // const fields = {};
-  // // This callback will be invoked for each file uploaded
-  // busboy.on("file", (name, file, info) => {
-  //   const { filename, encoding, mimetype } = info;
-  //   // console.log(
-  //   // `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
-  //   // );
-  //   const filepath = path.join(os.tmpdir(), filename);
-  //   upload = { file: filepath, type: mimetype };
-  //   file.pipe(fs.createWriteStream(filepath));
-  // });
-
-  // // This will invoked on every field detected
-  // busboy.on(
-  //   "field",
-  //   function (
-  //     fieldname,
-  //     val,
-  //     fieldnameTruncated,
-  //     valTruncated,
-  //     encoding,
-  //     mimetype
-  //   ) {
-  //     fields[fieldname] = val;
-  //   }
-  // );
-
-  // // This callback will be invoked after all uploaded files are saved.
-  // busboy.on("finish", () => {
-  //   var bucket = gcs.bucket("pwa-course-79727.appspot.com");
-  //   bucket.upload(
-  //     upload.file,
-  //     {
-  //       uploadType: "media",
-  //       metadata: {
-  //         metadata: {
-  //           contentType: upload.type,
-  //           firebaseStorageDownloadTokens: uuid,
-  //         },
-  //       },
-  //     },
-  //     function (err, uploadedFile) {
-  //       if (!err) {
-  //         admin
-  //           .database()
-  //           .ref("posts")
-  //           .push({
-  //             id: fields.id,
-  //             title: fields.title,
-  //             location: fields.location,
-  //             image:
-  //               "https://firebasestorage.googleapis.com/v0/b/" +
-  //               bucket.name +
-  //               "/o/" +
-  //               encodeURIComponent(uploadedFile.name) +
-  //               "?alt=media&token=" +
-  //               uuid,
-  //           })
-  //           .then(function () {
-  //             webpush.setVapidDetails(
-  //               "mailto:abhishekgoyal274@gmail.com",
-  //               "BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE",
-  //               "AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s"
-  //             );
-  //             return admin.database().ref("subscriptions").once("value");
-  //           })
-  //           .then(function (subscriptions) {
-  //             subscriptions.forEach(function (sub) {
-  //               var pushConfig = {
-  //                 endpoint: sub.val().endpoint,
-  //                 keys: {
-  //                   auth: sub.val().keys.auth,
-  //                   p256dh: sub.val().keys.p256dh,
-  //                 },
-  //               };
-
-  //               webpush
-  //                 .sendNotification(
-  //                   pushConfig,
-  //                   JSON.stringify({
-  //                     title: "New Post",
-  //                     content: "New Post added!",
-  //                     openUrl: "/help",
-  //                   })
-  //                 )
-  //                 .catch(function (err) {
-  //                   console.log(err);
-  //                 });
-  //             });
-  //             response
-  //               .status(201)
-  //               .json({ message: "Data stored", id: fields.id });
-  //           })
-  //           .catch(function (err) {
-  //             response.status(500).json({ error: err });
-  //           });
-  //       } else {
-  //         console.log(err);
-  //       }
-  //     }
-  //   );
-  // });
-
-  // // The raw bytes of the upload will be in request.rawBody.  Send it to busboy, and get
-  // // a callback when it's finished.
-  // busboy.end(request.rawBody);
-  // // formData.parse(request, function(err, fields, files) {
-  // //   fs.rename(files.file.path, "/tmp/" + files.file.name);
-  // //   var bucket = gcs.bucket("YOUR_PROJECT_ID.appspot.com");
-  // // });
 
 const port = 5000 || process.env.PORT;
 app.listen(port, () => {
