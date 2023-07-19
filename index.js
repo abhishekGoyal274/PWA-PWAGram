99e77bbfb8987d92b1cec0f5cd66b73e68dbc313
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-20 00:34:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+//Local server access
 const path = require("path");
+var fs = require("fs");
+//Image save
 const firebase = require("firebase-admin");
-const webpush = require("web-push");
+const uuid = require("uuid-v4");
 const multer = require("multer");
+//Notifications
+const webpush = require("web-push");
 
 // Database API
-const serviceAccount = require("/etc/secrets/pwa-course-79727.json");
+const serviceAccount = require("./pwa-course-79727.json");
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL:
     "https://pwa-course-79727-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "pwa-course-79727.appspot.com",
 });
 
 const app = express();
@@ -22,7 +28,7 @@ app.use(
     extended: true,
   })
 );
-app.use("/uploads", express.static("uploads"));
+
 const storageEngine = multer.diskStorage({
   destination: "./uploads",
   filename: (req, file, cb) => {
@@ -36,26 +42,52 @@ const checkFileType = function (file, cb) {
   if (mimeType && extName) {
     return cb(null, true);
   } else {
-    cb("Error: You can Only Upload Images!!");
+    cb("Error: You can Only Upload Images With extension jpeg ,jpg ,or png !!");
   }
 };
 const upload = multer({
   storage: storageEngine,
-  limits: { fileSize: 5000000 },
+  limits: { fileSize: 3000000 },
   fileFilter: (req, file, cb) => {
     checkFileType(file, cb);
   },
 });
 
-app.post("/storePostData", upload.single("file"), (request, response) => {
-  let image;
-  try {
-    image =
-      "https://" + request.headers.host + "/uploads/" + request.file.filename;
-  } catch (err) {
-    image = "https://" + request.headers.host + "/uploads/default.jpg";
-  }
+app.post("/storePostData", upload.single("file"), async (request, response) => {
   console.log(request.body);
+  var bucket = firebase.storage().bucket();
+  var filename = `uploads/${request.file.filename}`;
+  let image =
+    "https://firebasestorage.googleapis.com/v0/b/pwa-course-79727.appspot.com/o/unnamed.jpg?alt=media&token=41f64341-ae14-4a36-a2a6-01f5b341b3ba";
+  async function uploadFile() {
+    const metadata = {
+      metadata: {
+        firebaseStorageDownloadTokens: uuid(),
+      },
+      contentType: "image/jpg",
+      cacheControl: "public, max-age=31536000",
+    };
+
+    const data = await bucket.upload(filename, {
+      gzip: true,
+      metadata: metadata,
+    });
+    image = data[0].metadata.mediaLink;
+    console.log(`${filename} uploaded.`);
+  }
+  await uploadFile()
+    .then((res) => {
+      fs.unlink(`uploads/${request.file.filename}`, function (err) {
+        if (err) console.log(err);
+        else console.log("file deleted successfully");
+      });
+      // response.write("file uploaded");
+    })
+    .catch((error) => {
+      console.log("Error");
+      // response.send(error);
+    });
+
   firebase
     .database()
     .ref("posts")
@@ -63,9 +95,9 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
       id: request.body.id,
       title: request.body.title,
       location: request.body.location,
-      rawLocation : {
-        lat : request.body.rawLocationLat,
-        lng : request.body.rawLocationLng
+      rawLocation: {
+        lat: request.body.rawLocationLat,
+        lng: request.body.rawLocationLng,
       },
       image: image,
     })
@@ -73,7 +105,6 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
       return firebase.database().ref("subscriptions").once("value");
     })
     .then(function (subscriptions) {
-      console.log(subscriptions);
       subscriptions.forEach(function (sub) {
         var pushConfig = {
           endpoint: sub.val().endpoint,
@@ -82,16 +113,18 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
             p256dh: sub.val().keys.p256dh,
           },
         };
-        // const pubkey = "BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE";
-        // const privkey = "AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s";
+        const pubkey =
+          "BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE";
+        const privkey = "AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s";
         const options = {
           TTL: 24 * 60 * 60,
           vapidDetails: {
-            subject: 'https://pwa-course-79727.web.app',
-            publicKey: process.env.pubkey,
-            privateKey: process.env.privkey
+            subject: "https://pwa-course-79727.web.app",
+            publicKey: pubkey,
+            privateKey: privkey,
           },
-        }
+        };
+
         webpush
           .sendNotification(
             pushConfig,
@@ -103,20 +136,18 @@ app.post("/storePostData", upload.single("file"), (request, response) => {
             options
           )
           .then((res) => {
-            console.log("[Success]", res);
+            console.log("[Success]");
           })
           .catch(function (err) {
-            console.log("[Error in Web Push]",err);
+            console.log("[Error in Web Push]");
           });
       });
-      console.log("[Add Successfull]");
       response.status(201).json({
         message: "Data stored",
         id: request.body.id,
       });
     })
     .catch(function (err) {
-      console.log(err);
       response.status(500).json({ error: err });
     });
 });
